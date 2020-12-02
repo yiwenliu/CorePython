84840ce61d98e899d5a236d579dea418e1516e93
yiwen <sharmer@foxmail.com>
2020-12-02 23:33:09 +0800

    20201202

--- Chapter3/3.1-2.py
+++ Chapter3/3.1-2.py
@@ -1,15 +1,18 @@
 # -*- coding: utf-8 -*-
 # @Time : 2020/12/2 9:16
-# @Function : in运算符在字典中的应用
+# @Function : 字典的遍历
 
 if __name__ == "__main__":
     d = {'name': 'jason', 'age': '20'}
-    print("--遍历字典的keys--")
+    print("--遍历字典对象所有的键--")
     for key in d:
         print(key)
-    print("--遍历字典的keys--")
+    print("--遍历字典对象所有的键--")
     for key in d.keys():
         print(key)
-    print("--遍历字典的values--")
+    print("--遍历字典对象所有的values--")
     for value in d.values():
         print(value)
+    print("--遍历字典对象每个元素的key和value--")
+    for key, value in d.items():
+        print('%s is %s.' % (key, value))
