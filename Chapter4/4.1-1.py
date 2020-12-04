135d1479986aeb8609838bc83e0c78750b040196
刘一闻 <liuyiwen@xinhua.org>
2020-12-04 16:59:18 +0800

    1204

--- Chapter4/4.1-1.py
+++ Chapter4/4.1-1.py
@@ -1,15 +1,15 @@
 # -*- coding: utf-8 -*-
 # @Time : 2020/12/3 16:30
-# @Function :
+# @Function : bytes和str的区别
 
 if __name__ == "__main__":
     title = "python核心编程"
     print("--unicode字符--")
-    print('title是：', title)
     print('title的类型：', type(title))
+    print('title是：', title)
     print('title的长度：', len(title))
     print("--原始字节--")
-    title = title.encode('utf-8')
-    print('title是：', title)
-    print('title的类型：', type(title))
-    print('title的长度：', len(title))
+    title1 = title.encode('utf-8')
+    print('title1的类型：', type(title1))
+    print('title1是：', title1)
+    print('title1的长度：', len(title1))
