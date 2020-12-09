e55c84d2f49004d4e38b7f8b48e321de03e6b30d
刘一闻 <liuyiwen@xinhua.org>
2020-12-09 17:28:00 +0800

    1209

--- Chapter5/5.2-1.py
+++ Chapter5/5.2-1.py
@@ -1,30 +1,11 @@
 # -*- coding: utf-8 -*-
-# @Time : 2020/12/8 9:27
-# @Function : 自定义可迭代对象和迭代器
-
-
-class PowTwo:
-    """Class to implement an iterator
-    of powers of two"""
-
-    def __init__(self, max = 0):
-        self.max = max
-
-    def __iter__(self):
-        self.n = 0
-        return self
-
-    def __next__(self):
-        # 自定义class一定要有这条件语句，用来终止next()
-        if self.n <= self.max:
-            result = 2 ** self.n
-            self.n += 1
-            return result
-        else:
-            # 表示next()要终止
-            raise StopIteration
-
+# @Time : 2020/12/9 10:38
+# @Function : 内建数据结构的迭代器
 
 if __name__ == "__main__":
-    for i in PowTwo(5):
-        print(i)
+    x = [42, 23, 24]
+    it = iter(x)
+    print("列表迭代器的数据类型是： ", type(it))
+    print("列表x的下一个元素是：", next(it))
+    print("列表x的下一个元素是：", it.__next__())
+
---
+++ Chapter5/5.2-2.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+# @Time : 2020/12/8 9:27
+# @Function : 自定义可迭代对象和迭代器
+
+
+class PowTwo:
+    """Class to implement an iterator
+    of powers of two"""
+
+    def __init__(self, max = 0):
+        self.max = max
+
+    def __iter__(self):
+        self.n = 0
+        return self
+
+    def __next__(self):
+        # 自定义class一定要有这条件语句，用来终止next()
+        if self.n <= self.max:
+            result = 2 ** self.n
+            self.n += 1
+            return result
+        else:
+            # 表示next()要终止
+            raise StopIteration
+
+
+if __name__ == "__main__":
+    for i in PowTwo(5):
+        print(i)
