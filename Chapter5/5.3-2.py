7e226d67bee4a8cfa0a03a94a9a4db695e20e028
刘一闻 <liuyiwen@xinhua.org>
2020-12-16 19:18:25 +0800

    20201216

--- Chapter5/5.3-2.py
+++ Chapter5/5.3-2.py
@@ -2,23 +2,17 @@
 
 # A simple generator function
 def my_gen():
-    print('Start running the generator function......')
     n = 1
-    print('n =', n)
     yield n
 
     n += 1
-    print('n =', n)
     yield n
 
     n += 1
-    print('n =', n)
     yield n
 
 
 if __name__ == "__main__":
     a = my_gen()
-    next(a)
-    next(a)
-    next(a)
-    next(a)
+    for item in a:
+        print("n={}".format(item))
