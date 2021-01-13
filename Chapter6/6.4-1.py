d59e05b2897abff48ec185c3e6c09f1a7856e177
yiwenliu <sharmer@foxmail.com>
2021-01-13 09:51:22 +0800

    20210113

---
+++ Chapter6/6.4-1.py
@@ -0,0 +1,21 @@
+# -*- coding:UTF-8 -*-
+
+a = 1
+
+
+def g():
+    print("in function g: ",a)
+
+
+def f():
+    print("in function a, before: ", a)
+    a = 2
+    print("in function a, after: ", a)
+
+
+if __name__ == "__main__":
+    # g()
+    # f()
+    import dis
+    dis.dis(g)
+    dis.dis(f)
